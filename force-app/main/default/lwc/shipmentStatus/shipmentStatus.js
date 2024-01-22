752f0d69012874967786fd2bf0a5b6de8d847c63
Parul2661986 <157335594+Parul2661986@users.noreply.github.com>
2024-01-23 00:45:54 +0530

    Update shipmentStatus.js

--- force-app/main/default/lwc/shipmentStatus/shipmentStatus.js
+++ force-app/main/default/lwc/shipmentStatus/shipmentStatus.js
@@ -1,19 +1,27 @@
-import { LightningElement } from 'lwc';
-import doGet from '@salesforce/apex/MockShippingStatusService.doGet'
+import { LightningElement, track, api } from 'lwc';
+import getShipmentStatus from '@salesforce/apex/ShipmentStatusController.getShipmentStatus';
+
 export default class ShipmentStatus extends LightningElement {
-trackingStatus = '';
-error = '';
-connectedCallback(){
-getShipmentStatus();
-}
-getShipmentStatus(){
-doGet()
-.then((result) => {
-this.trackingStatus = result;
-console.log('result..'+this.trackingStatus);
-})
-.catch((error) => {
-this.error = error;
-});
-}
+    @api trackingNumber;
+    @track shipmentStatus;
+
+    handleTrackingNumberChange(event) {
+        this.trackingNumber = event.target.value;
+    }
+
+    getShipmentStatus() {
+        if (this.trackingNumber) {
+            getShipmentStatus({ trackingNumber: this.trackingNumber })
+                .then(result => {
+                    this.shipmentStatus = result;
+                })
+                .catch(error => {
+                    console.error('Error retrieving shipment status', error);
+                    this.shipmentStatus = 'Error - Unable to fetch shipment status.';
+                });
+        } else {
+            this.shipmentStatus = 'Please enter a tracking number.';
+        }
+    }
+    
 }
